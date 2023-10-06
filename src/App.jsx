99bd0ecde3ace910dc9168d6ee76cb90837f15ec
astarta16 <ngedeshuri2001@gmail.com>
2023-10-06 22:41:49 +0400

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Notification from './Components/Notifications'
+import Notification from "./Components/Notifications";
 
 function App() {
-    
- return(
-    <main className='bg-[#F7FAFD] h-[100vh] flex justify-center items-center'>
-        <Notification />
+  return (
+    <main className="bg-[#F7FAFD] h-[100vh] flex justify-center items-center">
+      <Notification />
     </main>
- );
+  );
 }
 
-export default App
+export default App;
--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -1,10 +1,12 @@
 export default function Notification() {
   return (
     <div className="bg-white w-[730px] pl-[32px] pt-[33px] pr-[30px] pb-[18px] rounded-[15px]">
-      <header className="flex justify-between">
-        <h1>
+      <header className="flex justify-between items-center">
+        <h1 className="text-[24px] font-[800]">
           Notifications
-          <span>3</span>
+          <span className="text-[16px] bg-[#0A327B] text-white px-[11px] py-[4px]  ml-1 rounded-[6px]">
+            3
+          </span>
         </h1>
         <p>Mark all as read</p>
       </header>
