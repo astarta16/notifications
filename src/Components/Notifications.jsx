9c02de52d28d8ed3eb95cd35a0e03f6a4c0274a7
astarta16 <ngedeshuri2001@gmail.com>
2023-10-07 14:01:51 +0400

    add differenc color in first 3 blocks

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -8,21 +8,28 @@ export default function Notification() {
       <header className="flex justify-between items-center">
         <h1 className="text-[24px] font-[800]">
           Notifications
-          <span className="text-[16px] bg-[#0A327B] text-white px-[11px] py-[4px]  ml-1 rounded-[6px]">
+          <span className="text-[16px] bg-[#0A327B] text-white px-[11px] py-[4px] ml-1 rounded-[6px]">
             3
           </span>
         </h1>
         <p>Mark all as read</p>
       </header>
       <div className="mt-[30px] flex flex-col gap-[20px]">
-        {userData.map((item) => {
+        {userData.map((item, index) => {
+          const isRead = item.read || index < 3; // Apply background color to the first 3 blocks or if 'item.read' is true
           return (
-            <div className="flex gap-[20px] items-center">
+            <div
+              className={`flex gap-[20px] items-center ${
+                isRead ? "bg-[#F7FAFD] p-3" : "bg-transparent"
+              }`}
+              key={index}
+            >
               <img
                 src={`./assets/avatar-${item.author
                   .replace(" ", "-")
                   .toLocaleLowerCase()}.webp`}
-                  className="w-[46px]"
+                className="w-[46px]"
+                alt={`${item.author}'s Avatar`}
               />
               <div>
                 <p>
@@ -30,7 +37,11 @@ export default function Notification() {
                   {" "}
                   <span>{item.type}</span>
                   {" "}
-                  <span>{item.content}</span>
+                  {item.content && item.content.includes(".webp") ? (
+                    <img src={item.content} alt="Notification Content" />
+                  ) : (
+                    <span>{item.content}</span>
+                  )}
                 </p>
                 <p>{item.time}</p>
               </div>
